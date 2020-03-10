c71cf575b86b6aa34f8b1b2842ded5b8c2140e6e
panqingchun <panqingchunmice@163.com>
2020-03-10 10:40:44 +0800

    Update F1-DH-125k.R

--- F1-DH-125k.R
+++ F1-DH-125k.R
@@ -1,15 +1,26 @@
+##### The script is to simulate the genotype of 200 F1-DH materials, and evaluate the number of recombination event and the size of recombinant fragments.
+##### Copyright Qingchun Pan, published on March 4, 2020
+
 rm(list=ls())
+
+#####  Define the physical length of different chromosome
+
 chrl<-c(307041676, 244442276, 235667822, 246994605, 223902239, 174033109, 182381541,181122619, 159769782, 150982312)
+
+#####  Divide into 125 kb segmentation of each chrosmome
+
 chrll<-round(chrl/1000000,0)
 chrtwo<-chrll*8
 
-pyy1<-function(pjj1){  
+pyy1<-function(pjj1){
 pww<-1:pjj1/8
 pww1<-pww*1000000
 pww2<-c(0,pww1[1:(length(pww1)-1)]+1)
 pww3<-cbind(pww2,pww1)
 pww3
 }
+
+##### Import recombiantion rate file based on the IBM genotype
 
 pfi<-read.table("map_data.txt",head=T,sep="\t")
 phh1<-NULL; phh2<-NULL;hhp1<-NULL
@@ -20,7 +31,6 @@ hhp1[[i]]<-cbind(i,phh1[[i]])
 }
 
 psss<-function(puu1,puu2){
-
 phh3<-list();phh4<-NULL;
 for (i in 1:nrow(puu1)){
 phh3[[i]]<-intersect(which(puu2[,4]>puu1[i,1]),which(puu2[,4]<puu1[i,2]))
@@ -42,8 +52,7 @@ pjjone[[i]]<-psss(phh1[[i]],phh2[[i]])
 
 final_pos<-do.call(rbind,hhp1)
 
-head(final_pos)
-
+#### To simulate two chromatids of 10 chromosomes in maize
 
 hh1<-NULL; hh2<-NULL;hh3<-NULL;ttt1<-NULL;ttt2<-NULL
 for  (i in 1:10){
@@ -53,25 +62,31 @@ hh3[[i]]<-rep(1,chrtwo[i])
 ttt1[[i]]<-cbind(hh1[[i]],hh2[[i]],hh3[[i]])
 ttt2[[i]]<-cbind(i,ttt1[[i]],pjjone[[i]][,2])
 }
+
 ttt3<-do.call(rbind,ttt2)
+
+#### Define the probability of recombination event  for each generation on each chromosome 
 
 jj<-cbind(c(0:3),c(0.16,0.39,0.32,0.13)) 
 
+#### Define the funciton of two times recombinaiton on each chromosome
+
 sss2<-function(niu){ 
- 
 for (i in 1:3000)
 {
-www<-sample(niu[2:(nrow(niu)-2),1],2,pro=niu[2:(nrow(niu)-2),4]) #### hh1 #### sample(niu,2)
+www<-sample(niu[2:(nrow(niu)-2),1],2,pro=niu[2:(nrow(niu)-2),4]) 
 www1<-sort(www)
 ww1<-(www1[2]-www1[1])
 ww2<-NULL
-if(ww1>10){  
+if(ww1>10){  ##### Threshold setting of recombination interference and linkage 
 ww2<-www1
 }
 break
 }
 ww2   
 }
+
+####  Define the function of three times recombination on each chromosome
 
 sss3<-function(niu1){ 
 for (i in 1:3000)
@@ -81,7 +96,7 @@ www1<-sort(www)
 ww1<-(www1[2]-www1[1])
 ww1_2<-(www1[3]-www1[2])
 ww3<-NULL
-if (ww1>10 && ww1_2>10){  
+if (ww1>10 && ww1_2>10){  ##### Threshold setting of recombination interference and linkage 
 ww3<-www1
 }
 break
@@ -89,45 +104,44 @@ break
 ww3    
 }
 
-uuu<-function(rrr){
+#### Recombination event on chromosome
 
-oo<-sample(jj[,1],1,pro=jj[,2])
-##oo<-1
-if (oo==0){ 
-se<-sample(c(1,2),1)
+uuu<-function(rrr){
+oo<-sample(jj[,1],1,pro=jj[,2])  #### Random selection of recombination times (0,1,2,3)
+if (oo==0){ ###  No recombination
+se<-sample(c(1,2),1)  ####  Random selection of a gamete between two parents
 fu<-rrr[,1+se] 
 }
-
-if(oo==1){  
-se<-sample(c(1,2),1)
+if(oo==1){  ###  One time recombination
+se<-sample(c(1,2),1)  #### Random selection of a gamete between two parents
 fk_1<-sample(rrr[,1][2:(nrow(rrr)-1)],1,pro=rrr[2:(nrow(rrr)-1),4])
 if (se==1){
 fu<-c(rrr[1:fk_1,2],rrr[(fk_1+1):nrow(rrr),3])}else{
 fu<-c(rrr[1:fk_1,3],rrr[(fk_1+1):nrow(rrr),2])
 }
 }
-if(oo==2){ 
+if(oo==2){  ### Two time recombinations
 pp1<-NULL;pp2<-NULL
 for (i in 1:100){
 pp1[[i]]<-sss2(rrr) 
 }
 pp2<-do.call(rbind,pp1)
 pp3<-pp2[sample(1:nrow(pp2),1),] 
-se<-sample(c(1,2),1)
+se<-sample(c(1,2),1) ####  Random selection of a gamete between two parents
 if (se==1){
 fu<-c(rrr[1:pp3[1],2],rrr[(pp3[1]+1):pp3[2],3],rrr[(pp3[2]+1):nrow(rrr),2])}else{
-fu<-c(rrr[1:pp3[1],3],rrr[(pp3[1]+1):pp3[2],2],rrr[(pp3[2]+1):nrow(rrr),3])   #######  this is error ########## 
+fu<-c(rrr[1:pp3[1],3],rrr[(pp3[1]+1):pp3[2],2],rrr[(pp3[2]+1):nrow(rrr),3])  
 }
 }
 
-if(oo==3){ 
+if(oo==3){  ### Two time recombinations
 pp1_1<-NULL;pp2_1<-NULL
 for (i in 1:100){
 pp1_1[[i]]<-sss3(rrr)  
 }
 pp2_1<-do.call(rbind,pp1_1)
 pp3_1<-pp2_1[sample(1:nrow(pp2_1),1),]
-se<-sample(c(1,2),1)
+se<-sample(c(1,2),1) ####  Random selection of a gamete between two parents
 if (se==1){
 fu<-c(rrr[1:pp3_1[1],2],rrr[(pp3_1[1]+1):pp3_1[2],3],rrr[(pp3_1[2]+1):pp3_1[3],2],rrr[(pp3_1[3]+1):nrow(rrr),3])}else{
 fu<-c(rrr[1:pp3_1[1],3],rrr[(pp3_1[1]+1):pp3_1[2],2],rrr[(pp3_1[2]+1):pp3_1[3],3],rrr[(pp3_1[3]+1):nrow(rrr),2])
@@ -137,8 +151,10 @@ fu_1<-cbind(rrr[,1],fu,rrr[,4])
 fu_1 
 }
 
+####  Ten chromosomes
+
 lilinone<-function(tttt){
-lele<-tttt  
+lele<-tttt 
 le_1<-NULL; le_2<-NULL
 for (i in 1:10){
 le_1[[i]]<-lele[which(lele[,1]==i),]
@@ -151,6 +167,8 @@ geng[[i]]<-cbind(i,uuu(le_2[[i]]))
 jby<-do.call(rbind,geng)
 jby
 }
+
+####  200 lines
 
 nayiye<-function(nanana){
 DH_200<-NULL;
@@ -167,59 +185,11 @@ for (i in 1:200){
 id_yy[i]<-4*i-1
 }
 
+#### Genotype of 200 lines  
+
 fiss_1<-cbind(final_pos,fiss[,id_yy])
+colnames(fiss_1)<-c("Chr","Phy_start","Phy_end",paste("L",1:200,sep=""))
+write.table("F1-DH-125k-genotype.txt",col.names=T,row.names=F,sep="\t",quote=F) ###  the result file of genotype with 200 lines 
 
-issi<-fiss_1[,4:ncol(fiss_1)]
-issi_1<-apply(issi,1,table)
-iss_rate<-issi_1[1]/200
 
-tjone<-function(fiii){
-iss<-NULL
-iss<-which(fiii[1:(length(fiii)-1)]!=fiii[2:length(fiii)])
-isss<-length(iss)
-isss
-}
 
-pdss<-cbind(fiss_1[,1],issi)
-pdd<-function(pdd11){
-tj<-NULL;tjj<-NULL
-for (i in 1:10){
-tj[[i]]<-pdd11[which(pdd11[,1]==i),]
-tjj[[i]]<-tjone(tj[[i]][,2])
-}
-tjsu<-sum(unlist(tjj))
-tjsu
-}
-kktj<-NULL
-for (i in 1:200){
-kktj[[i]]<-pdd(pdss[,c(1,i+1)])
-}
-mean(kktj) 
-
-fiii<-fiss_1; tete<-NULL;tete1<-NULL
-for (i in 1:10){
-tete[[i]]<-fiii[which(fiii[,1]==i),]
-tete1[[i]]<-tete[[i]][,4:ncol(tete[[i]])]
-}
-
-finnn<-function(ninini,ninini1){
-fiii_1<-ninini;fififi<-ninini1;
-iss<-NULL
-for (i in 1:nrow(fiii_1)){
-iss[i]<-paste(fiii_1[i,],collapse="")
-}
-issout<-NULL
-for (i in 1:(nrow(fiii_1)-1)){
-issout[i]<-(iss[i]==iss[i+1])
-}
-issout1<-which(issout==FALSE)
-issout2<-cbind(c(1,issout1),c(issout1,nrow(fiii_1)))
-issout3<-fififi[issout2[,2],3]-fififi[issout2[,1],2]
-issout3
-}
-leng_re<-NULL
-for (i in 1:10){
-leng_re[[i]]<-finnn(tete1[[i]],tete[[i]])
-}
-leng_re1<-unlist(leng_re)
-mean(leng_re1)
